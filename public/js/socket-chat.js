9c45b8bcb6161779b1506327bc4a37aeb22bab93
Christopher Aedo <Christopher.aedo.p@gmail.com>
2018-04-30 20:47:26 -0300

    chat primera version

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -3,26 +3,27 @@ var socket = io();
 var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
-	window.location = 'index.html';
-	throw new Error('El nombre y sala es necesario');
+    window.location = 'index.html';
+    throw new Error('El nombre y sala es necesario');
 }
 
 var usuario = {
-	nombre: params.get('nombre'),
-	sala: params.get('sala')
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
 };
 
 socket.on('connect', function() {
-	console.log('Conectado al servidor');
+    console.log('Conectado al servidor');
 
-	socket.emit('entrarChat', usuario, function(resp) {
-		console.log('usuarios conectados', resp);
-	});
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('usuarios conectados', resp);
+        renderizarUsuario(resp);
+    });
 });
 
 // escuchar
 socket.on('disconnect', function() {
-	console.log('Perdimos conexión con el servidor');
+    console.log('Perdimos conexión con el servidor');
 });
 
 // Enviar información
@@ -38,13 +39,16 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-	console.log('Servidor:', mensaje);
+    console.log('Servidor:', mensaje);
+    renderizarMensaje(mensaje, false);
+    scrollBottom();
 });
 // Escuchar información
 socket.on('listaPersonas', function(personas) {
-	console.log('Servidor:', personas);
+    console.log('Servidor:', personas);
+    renderizarUsuario(personas);
 });
 // Mensajes Privados
 socket.on('mensajePrivado', function(mensaje) {
-	console.log('Mensaje Privado:', mensaje);
+    console.log('Mensaje Privado:', mensaje);
 });
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -5,66 +5,78 @@ const { Usuarios } = require('../classes/usuarios');
 const usuarios = new Usuarios();
 
 io.on('connection', client => {
-	client.on('entrarChat', (data, callback) => {
-		if (!data.nombre || !data.sala) {
-			return callback({
-				error: true,
-				mensaje: 'El nombre y sala es necesario'
-			});
-		}
+    client.on('entrarChat', (data, callback) => {
+        if (!data.nombre || !data.sala) {
+            return callback({
+                error: true,
+                mensaje: 'El nombre y sala es necesario'
+            });
+        }
 
-		client.join(data.sala);
+        client.join(data.sala);
 
-		usuarios.agregarPersona(client.id, data.nombre, data.sala);
+        usuarios.agregarPersona(client.id, data.nombre, data.sala);
 
-		client.broadcast
-			.to(data.sala)
-			.emit('listaPersonas', usuarios.getPersonasPorSala(data.sala));
+        client.broadcast
+            .to(data.sala)
+            .emit('listaPersonas', usuarios.getPersonasPorSala(data.sala));
 
-		callback(usuarios.getPersonasPorSala(data.sala));
-	});
+        client.broadcast
+            .to(data.sala)
+            .emit(
+                'crearMensaje',
+                crearMensaje(
+                    'Administrador',
+                    `${data.nombre} ingreso el chat`
+                )
+            );
 
-	client.on('crearMensaje', data => {
-		let persona = usuarios.getPersona(client.id);
+        callback(usuarios.getPersonasPorSala(data.sala));
+    });
 
-		let mensaje = crearMensaje(persona.nombre, data.mensaje);
+    client.on('crearMensaje', (data, callback) => {
+        let persona = usuarios.getPersona(client.id);
 
-		client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
-	});
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
 
-	// mensajes Privados
-	client.on('mensajePrivado', data => {
-		if (!data.para) {
-			return callback({
-				error: true,
-				mensaje: 'El id del usuario es necesario'
-			});
-		}
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
 
-		let persona = usuarios.getPersona(client.id);
-		client.broadcast
-			.to(data.para)
-			.emit('crearMensaje', crearMensaje(persona.nombre, data.mensaje));
-	});
+        callback(mensaje)
+    });
 
-	client.on('disconnect', () => {
-		let personaBorrada = usuarios.borrarPersona(client.id);
+    // mensajes Privados
+    client.on('mensajePrivado', data => {
+        if (!data.para) {
+            return callback({
+                error: true,
+                mensaje: 'El id del usuario es necesario'
+            });
+        }
 
-		client.broadcast
-			.to(personaBorrada.sala)
-			.emit(
-				'crearMensaje',
-				crearMensaje(
-					'Administrador',
-					`${personaBorrada.nombre} abondono el chat`
-				)
-			);
+        let persona = usuarios.getPersona(client.id);
+        client.broadcast
+            .to(data.para)
+            .emit('crearMensaje', crearMensaje(persona.nombre, data.mensaje));
+    });
 
-		client.broadcast
-			.to(personaBorrada.sala)
-			.emit(
-				'listaPersonas',
-				usuarios.getPersonasPorSala(personaBorrada.sala)
-			);
-	});
+    client.on('disconnect', () => {
+        let personaBorrada = usuarios.borrarPersona(client.id);
+
+        client.broadcast
+            .to(personaBorrada.sala)
+            .emit(
+                'crearMensaje',
+                crearMensaje(
+                    'Administrador',
+                    `${personaBorrada.nombre} abondono el chat`
+                )
+            );
+
+        client.broadcast
+            .to(personaBorrada.sala)
+            .emit(
+                'listaPersonas',
+                usuarios.getPersonasPorSala(personaBorrada.sala)
+            );
+    });
 });
